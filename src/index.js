e8e63de4537bcbe4164dd37cf0761717299361cf
mhmdrdat <Mohammad Reza>
2024-03-26 00:28:27 +0330

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
 import { router } from './routes/route';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <HelmetProvider>
     <RouterProvider router={router} />
-    </HelmetProvider>
   </StrictMode>
 );
 
