8a632c22da1b8be265a8c4604423e404a81a413c
mhmdrdat <Mohammad Reza>
2024-03-25 23:54:54 +0330

    init app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './fonts/IranSans/Webfonts/fontiran.css'
+import './App.css'
+const App = () => {
+  return (
+    <>
+    <h2>
+      سلام دنیا
+    </h2>
+    </>
+  )
+}
+
+export default App;
